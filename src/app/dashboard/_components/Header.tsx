f38a4f615961cbbdcfec8c2eb979ee200e289b9c
imsidkg <siddharth8271skg2@gmail.com>
2024-09-16 02:24:30 +0530

    clerk auth added

--- src/app/dashboard/_components/Header.tsx
+++ src/app/dashboard/_components/Header.tsx
@@ -15,7 +15,7 @@ const Header: React.FC = () => {
             <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard' && ' font-bold'}`}>Dashboard</li>
             <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/questions' && 'text-primary font-bold'}`}>Questions</li>
             <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/upgrade' && 'text-primary font-bold'}`}>Upgrade</li>
-            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/how' && 'text-primary font-bold'}`}>How its works?</li>
+            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/how' && 'text-primary font-bold'}`}>How it works?</li>
         </ul>
     </div>
   )
